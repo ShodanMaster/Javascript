6d2c87d9d6ab230736223265c14231a6b10b56f9
ShodanMaster <akshaykrishnan922@gmail.com>
2025-06-11 23:56:08 +0530

    01

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 341
+let accountEmail = "akshay@examle.com"
+var accountPassword = "akshay@example"
+accountCity = "Cherupuzha"
+
+// accountId = 2 // can not assign value to const variable which is already assigned as const
+
+console.log(accountId);
+
+accountEmail = "kannan@example.com"
+accountPassword = "kannan@example"
+accountCity = "Payyannur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
