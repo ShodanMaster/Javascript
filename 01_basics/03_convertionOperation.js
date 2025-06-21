d608693e7c61ca9d4f1d9d17c7173a3131d932e3
ShodanMaster <akshaykrishnan922@gmail.com>
2025-06-21 16:44:14 +0530

    convertions

--- 01_basics/03_convertionOperation.js
+++ 01_basics/03_convertionOperation.js
@@ -10,3 +10,17 @@ console.log(typeof valuInScoreString); // number
 console.log(valuInScore); // 33
 console.log(valuInScoreString); // NaN (Not a Number)
 //value of null is 0;
+
+/* *********************** Operations *********************** */
+
+console.log(1 + 2); // 3
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + "2"); // 12
+console.log("1" + "2" + 2); // 122
+console.log(1 + 2 + "2"); // 32
+console.log(1 + 2 + "2" + 3); // 3223
+
+console.log(+true); // 1
+console.log(+false); // 0
+console.log(+""); // 0
